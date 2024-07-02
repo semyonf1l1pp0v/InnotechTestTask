bc5e42e090d709c6ea8e77eb4ba7404fa6735c5f
semyonf1l1pp0v <syomchik13@gmail.com>
2024-07-02 22:50:28 +0300

    authentication form and basic username/password check added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse, HTMLResponse
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -11,7 +11,17 @@ def homepage():
     return html_response
 
 
-@app.get("/login")
-def redirect_to_login():
-    return PlainTextResponse("You have been successfully redirected")
+@app.get("/login", response_class=HTMLResponse)
+def authpage():
+    with open("authform.html", "r") as f:
+        html_response = f.read()
+    return html_response
+
+
+@app.post("/login")
+def login(username: str = Form(...), password: str = Form(...)):
+    if username == 'admin' and password == 'admin':
+        return "Hell yeah"
+    else:
+        return "Not hell yeah"
 
