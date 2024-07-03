eabe98b30bfdb8a4603a3f0b511b9e49a52e5906
semyonf1l1pp0v <syomchik13@gmail.com>
2024-07-04 00:02:18 +0300

    basic functionality to find appsec practice added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
 import bcrypt
@@ -36,6 +38,41 @@ def login(username: str = Form(...), password: str = Form(...)):
         return open_html("incorrect_cred.html")
 
 
+appsec_practices = {
+    "SAST": "SAST helps detect code flaws by analyzing the application source files for root causes. It enables "
+            "comparing static analysis scan results with real-time solutions to quickly detect security problems, "
+            "decrease the mean time to repair (MTTR), and troubleshoot collaboratively.",
+    "DAST": "DAST is a proactive testing approach that simulates security breaches on a running web application to "
+            "identify exploitable flaws. These tools evaluate applications in production to help detect runtime or "
+            "environment-related errors.",
+    "IAST": "IAST utilizes SAST and DAST elements, performing analysis in real-time or at any SDLC phase from within "
+            "the application. IAST tools get access to the application's code and components, which means the tools "
+            "achieve the in-depth access needed to produce accurate results.",
+    "RASP": "RASP tools work within the application to provide continuous security checks and automatically respond "
+            "to possible breaches. Common responses include alerting IT teams and terminating a suspicious session.",
+    "MAST": "MAST tools test the security of mobile applications using various techniques, such as performing static "
+            "and dynamic analysis and investigating forensic data gathered by mobile applications. MAST tools help "
+            "identify mobile-specific issues and security vulnerabilities, such as malicious WiFi networks, "
+            "jailbreaking, and data leakage from mobile devices.",
+    "WAF": "A WAF solution monitors and filters all HTTP traffic passing between the Internet and a web application. "
+           "These solutions do not cover all threats. Rather, WAFs work as part of a security stack that provides a "
+           "holistic defense against the relevant attack vectors. WAF works as a protocol layer seven defense when "
+           "applied as part of the open systems interconnection (OSI) model. It helps protect web applications "
+           "against various attacks, including cross-site-scripting (XSS), SQL injection (SQLi), file inclusion, "
+           "and cross-site forgery (CSRF).",
+    "CNAPP": "A cloud native application protection platform (CNAPP) centralizes the control of all tools used to "
+             "protect cloud native applications. It unifies various technologies, such as cloud security posture "
+             "management (CSPM) and cloud workload protection platform (CWPP), identity entitlement management, "
+             "automation and orchestration security for container orchestration platforms like Kubernetes, "
+             "and API discovery and protection."
+}
+
+
 @app.get("/appsec")
-def appsec_main():
-    return open_html("appsec.html")
+def get_practice(key: Optional[str] = None):
+    if not key:
+        return open_html("appsec.html")
+    for el in appsec_practices.items():
+        if el[0].lower() == key.lower():
+            return {el[0]:el[1]}
+    return {"error": "key not found"}
