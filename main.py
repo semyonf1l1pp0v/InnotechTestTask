91295412d77d8e6e1109e0bb075bfd5a40c93695
semyonf1l1pp0v <syomchik13@gmail.com>
2024-07-03 18:04:41 +0300

    something similar to auth with token done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form, Depends
+from fastapi import FastAPI, Form, Depends, HTTPException, status, Response
 from fastapi.responses import HTMLResponse
 from fastapi.security import OAuth2PasswordBearer
 import jwt
@@ -14,17 +14,6 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 
 
-def check_password(plain_password, hashed_password):
-    return bcrypt.checkpw(plain_password, hashed_password)
-
-
-def authenticate_user(username, plain_password, hashed_password):
-    if username == users_db["admin"]["username"] and check_password(plain_password, hashed_password):
-        return users_db["admin"]["username"]
-    else:
-        return False
-
-
 def create_access_token(data):
     to_encode = data.copy()
     jwt_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -32,6 +21,8 @@ def create_access_token(data):
 
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -49,8 +40,30 @@ def authpage():
 
 
 @app.post("/login")
-def login(username: str = Form(...), password: str = Form(...)):
-    if username == 'admin' and password == 'admin':
-        return "Hell yeah"
+def login(response: Response, username: str = Form(...), password: str = Form(...)):
+    if username == users_db["admin"]["username"] and bcrypt.checkpw(password.encode(),
+                                                                    users_db["admin"]["hashed_password"]):
+        print("Success")
+        access_token = create_access_token(data={"sub": users_db["admin"]["username"]})
+        response.headers["Authorization"] = f"Bearer {access_token}"
+        return content(token=access_token)
     else:
-        return "Not hell yeah"
+        return False
+
+
+@app.get("/content")
+def content(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        token_data = username
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return f"Hi {token_data}"
+
