18ec3d59d5733b0bbde21e89f1cd03736077ba3d
semyonf1l1pp0v <syomchik13@gmail.com>
2024-07-03 19:27:02 +0300

    proceed without token, maybe add later

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from fastapi import FastAPI, Form, Depends, HTTPException, status, Response
+from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
-from fastapi.security import OAuth2PasswordBearer
-import jwt
 import bcrypt
 
 users_db = {"admin":
@@ -10,60 +8,34 @@ users_db = {"admin":
                  }
             }
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-
-
-def create_access_token(data):
-    to_encode = data.copy()
-    jwt_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return jwt_token
-
-
 app = FastAPI()
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 
 @app.get("/", response_class=HTMLResponse)
 def homepage():
-    with open("homepage.html", "r") as f:
-        html_response = f.read()
+    with open("homepage.html") as file:
+        html_response = file.read()
     return html_response
 
 
 @app.get("/login", response_class=HTMLResponse)
 def authpage():
-    with open("authform.html", "r") as f:
-        html_response = f.read()
+    with open("authform.html") as file:
+        html_response = file.read()
     return html_response
 
 
 @app.post("/login")
-def login(response: Response, username: str = Form(...), password: str = Form(...)):
+def login(username: str = Form(...), password: str = Form(...)):
     if username == users_db["admin"]["username"] and bcrypt.checkpw(password.encode(),
                                                                     users_db["admin"]["hashed_password"]):
-        print("Success")
-        access_token = create_access_token(data={"sub": users_db["admin"]["username"]})
-        response.headers["Authorization"] = f"Bearer {access_token}"
-        return content(token=access_token)
+        pass
     else:
-        return False
+        with open("homepage.html") as file:
+            html_response = file.read()
+        return HTMLResponse(content=html_response)
 
 
-@app.get("/content")
-def content(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-        token_data = username
-    except jwt.PyJWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return f"Hi {token_data}"
-
+@app.get("/appsec")
+def appsec_main():
+    return "Appsec here"
